b065bd8eb6decedb699a2f4f38ce72b71371f700
oumarichard <richard.ouma@student.moringaschool.com>
2022-11-02 13:08:49 +0300

    Completed asiignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+#Database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db.dogs.sqlite"
+)
+
+#creatting students table
+
+sql =<<-SQL
+CREATE TABLE IF NOT EXISTS dogs(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+)
+SQL
+
+class Dog < ActiveRecord::Base
 end
